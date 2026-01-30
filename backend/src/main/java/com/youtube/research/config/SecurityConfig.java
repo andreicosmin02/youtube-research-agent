568aa6121fc25abd355fe3043219d51e3b197028
andreicosmin02 <andrei.achim.6669@gmail.com>
2026-01-30 10:23:59 +0200

    Small fixes

--- backend/src/main/java/com/youtube/research/config/SecurityConfig.java
+++ backend/src/main/java/com/youtube/research/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.youtube.research.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-// Remove the duplicate import if it was there
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
